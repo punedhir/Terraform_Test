604659377659b191eff959ca250b4f5a0ba26f83
puneet <Puneet@who.com>
2019-01-22 18:33:13 +0530

    Puppet Scripts

--- azure/puppetagent.sh
+++ azure/puppetagent.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 wget https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
-sudo -s  dpkg -i puppetlabs-release-pc1-xenial.deb
-sudo -s  apt-get --asssume-yes update
-sudo -s  apt-get --asssume-yes install puppet-agent
-echo "Puppet Agent completed"
-
+dpkg -i puppetlabs-release-pc1-xenial.deb
+apt-get update -y
+apt-get install puppet-agent -y
 
 
--- azure/puppetmaster.sh
+++ azure/puppetmaster.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 curl -O https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
-sudo -s dpkg -i puppetlabs-release-pc1-xenial.deb
-echo "depackage the puppet4" 
-sudo -s apt-get --assume-yes update
-sudo -s apt-get --assume-yes install puppetserver
-sudo -s ufw allow 8140
-sudo -s systemctl start puppetserver
-echo "Puppet SErver has been started"
+dpkg -i puppetlabs-release-pc1-xenial.deb
+apt-get update -y
+apt-get install puppetserver -y
 
-
-
-curl -O https://apt.puppetlabs.com/puppetlabs-release-pc1-xenial.deb
-sudo -s dpkg -i puppetlabs-release-pc1-xenial.deb
-sudo -s apt-get --assume-yes update
-sudo -s apt-get --assume-yes install puppetserver
-sudo -s ufw allow 8140
-sudo -s systemctl start puppetserver
