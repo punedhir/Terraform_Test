e2d0c57ed8c0fc03618a083d9d32226055bb90d8
punedhir <42438754+punedhir@users.noreply.github.com>
2019-04-26 13:28:27 +0100

    Kubernetes Node IP Flag

--- azure/kubemaster.sh
+++ azure/kubemaster.sh
@@ -9,6 +9,9 @@ apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
 apt install -y docker.io
 systemctl start kubelet
+host=`ip addr show eth0 | grep -w inet|awk '{print substr($2,0,length($2)-3)}' | head -1`
+sudo sed -i "/KUBELET_CONFIG_ARGS=/a Environment=\"KUBELET_EXTRA_ARGS=--node-ip=$host\"" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
 
 
 
